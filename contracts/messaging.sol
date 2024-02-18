870499913e069d7171a73f33ed2b7e23f4e2fdba
Jacobo149 <jtp323@lehigh.edu>
2024-02-18 16:16:09 -0500

    Contracts Created

--- contracts/messaging.sol
+++ contracts/messaging.sol
@@ -11,3 +11,21 @@ getmessagebyindex()
 
 
  */
+
+pragma solidity ^0.8.21;
+
+contract Messaging {
+    mapping (address => mapping (address => string[])) public messages;
+
+    function sendMessage(address _recipient, string memory _message) public {
+        messages[msg.sender][_recipient].push(_message);
+    }
+
+    function getMessages(address _sender, address _recipient) public view returns (string[] memory) {
+        return messages[_sender][_recipient];
+    }
+
+    function getMessageByIndex(address _sender, address _recipient, uint _index) public view returns (string memory) {
+        return messages[_sender][_recipient][_index];
+    }
+}
--- contracts/storage.sol
+++ contracts/storage.sol
@@ -9,3 +9,17 @@ storefile()
 retrievefile()
 
  */
+
+pragma solidity ^0.8.21;
+
+contract Storage {
+    mapping (bytes32 => bytes) public files;
+
+    function storeFile(bytes32 _fileHash, bytes memory _fileData) public {
+        files[_fileHash] = _fileData;
+    }
+
+    function retrieveFile(bytes32 _fileHash) public view returns (bytes memory) {
+        return files[_fileHash];
+    }
+}
